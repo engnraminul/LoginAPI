9cc63965d3860b5dd91d4f9d98b6c3ee1f108c26
engnraminul <engnraminul@gmail.com>
2022-05-15 17:11:29 +0600

    setup all task in project

---
+++ LoginAPI/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib. staticfiles.urls import static, staticfiles_urlpatterns
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_api'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_api')
+]
+urlpatterns +=staticfiles_urlpatterns()
+urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
