f86b4ac460f52a3c3d0b6fb0d3f3f90e48e5b373
engnraminul <engnraminul@gmail.com>
2022-05-16 12:34:09 +0600

    Create Register Serializer and View

--- LoginAPI/urls.py
+++ LoginAPI/urls.py
@@ -1,6 +1,7 @@
 
+from django import urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib. staticfiles.urls import static, staticfiles_urlpatterns
@@ -9,7 +10,8 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_api'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_api')
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_api'),
+    path('account/', include('Account.urls'))
 ]
 urlpatterns +=staticfiles_urlpatterns()
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
